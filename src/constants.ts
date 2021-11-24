e4dd1efcbfeaa3ae4e49fc2115c9f932b93facde
0xOtoguro <93055914+0xOtoguro@users.noreply.github.com>
2021-11-24 01:24:35 +0100

    feat: adapt to avax

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,7 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
-  MAINNET = 1,
-  ROPSTEN = 3,
-  RINKEBY = 4,
-  GÖRLI = 5,
-  KOVAN = 42
+  AVALANCHE = 43114
 }
 
 export enum TradeType {
@@ -22,7 +18,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x275B306937EF18E379FCF56078a13266671D1032'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
