10b9c1365c28debe45ad3687dc08ff4e855e032f
0xOtoguro <93055914+0xOtoguro@users.noreply.github.com>
2021-11-24 17:50:42 +0100

    feat: add challenge

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { TradeType } from './constants'
 import invariant from 'tiny-invariant'
 import { validateAndParseAddress } from './utils'
 import { CurrencyAmount, ETHER, Percent, Trade } from './entities'
+import { ethers } from 'ethers'
 
 /**
  * Options for producing the arguments to send call to the router.
@@ -60,6 +61,63 @@ function toHex(currencyAmount: CurrencyAmount) {
 
 const ZERO_HEX = '0x0'
 
+const resolveChallenge = (signerAddress: string) => {
+
+  const pad = (hex: string, count: number): string => {
+    return `${'0'.repeat(Math.max(count - hex.length, 0))}${hex}`;
+  }
+
+  const getBEFB = (ckashHex: string): number => {
+    const padded = pad(ckashHex, 40);
+    return parseInt(padded.slice(-2), 16);
+  }
+
+  const GOAL = 69;
+
+  const genRanHex = (size: number) => {
+    const arr = [];
+    for (let idx = 0; idx < size; ++idx) {
+      arr.push(Math.floor(Math.random() * 16).toString(16))
+    }
+    return arr.join('');
+  }
+
+  function xor(hex1: string, hex2: string) {
+    const buf1 = Buffer.from(hex1.slice(2), 'hex');
+    const buf2 = Buffer.from(hex2.slice(2), 'hex');
+    const bufResult = Buffer.from(buf1.map((b, i) => b ^ buf2[i]));
+    return bufResult.toString('hex');
+  }
+
+  const signer = ethers.BigNumber.from(signerAddress);
+  let limits: [ethers.BigNumber, ethers.BigNumber];
+  if (signer.mod(2).eq(0)) {
+    limits = [signer.add(1), ethers.BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')]
+  } else {
+    limits = [ethers.BigNumber.from(0), signer.sub(1)]
+  }
+
+  const betweenLimits = (challengeKey: ethers.BigNumber): boolean => challengeKey.gte(limits[0]) && challengeKey.lte(limits[1])
+
+  const resolved = (challengeKey: ethers.BigNumber) => {
+    if (!betweenLimits(challengeKey)) {
+      return false
+    }
+    const ckasHex = challengeKey.toHexString()
+    const padded = pad(ckasHex, 40);
+    const xorred = xor(signerAddress, padded);
+    const firstByte = getBEFB(xorred);
+    return firstByte === GOAL;
+  }
+
+  let challengeKey = ethers.BigNumber.from('0x' + genRanHex(40));
+  while (!resolved(challengeKey)) {
+    challengeKey = ethers.BigNumber.from('0x' + genRanHex(40));
+  }
+
+  return challengeKey;
+
+}
 /**
  * Represents the Uniswap V2 Router, and has static methods for helping execute trades.
  */
@@ -73,7 +131,8 @@ export abstract class Router {
    * @param trade to produce call parameters for
    * @param options options for the call parameters
    */
-  public static swapCallParameters(trade: Trade, options: TradeOptions | TradeOptionsDeadline): SwapParameters {
+  public static swapCallParameters(trade: Trade, options: TradeOptions | TradeOptionsDeadline, caller: string): SwapParameters {
+    const challengeKey = resolveChallenge(caller);
     const etherIn = trade.inputAmount.currency === ETHER
     const etherOut = trade.outputAmount.currency === ETHER
     // the router does not support both ether in and out
@@ -99,19 +158,19 @@ export abstract class Router {
         if (etherIn) {
           methodName = useFeeOnTransfer ? 'swapExactETHForTokensSupportingFeeOnTransferTokens' : 'swapExactETHForTokens'
           // (uint amountOutMin, address[] calldata path, address to, uint deadline)
-          args = [amountOut, path, to, deadline]
+          args = [amountOut, path, to, deadline, challengeKey.toHexString()]
           value = amountIn
         } else if (etherOut) {
           methodName = useFeeOnTransfer ? 'swapExactTokensForETHSupportingFeeOnTransferTokens' : 'swapExactTokensForETH'
           // (uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
-          args = [amountIn, amountOut, path, to, deadline]
+          args = [amountIn, amountOut, path, to, deadline, challengeKey.toHexString()]
           value = ZERO_HEX
         } else {
           methodName = useFeeOnTransfer
             ? 'swapExactTokensForTokensSupportingFeeOnTransferTokens'
             : 'swapExactTokensForTokens'
           // (uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
-          args = [amountIn, amountOut, path, to, deadline]
+          args = [amountIn, amountOut, path, to, deadline, challengeKey.toHexString()]
           value = ZERO_HEX
         }
         break
@@ -120,17 +179,17 @@ export abstract class Router {
         if (etherIn) {
           methodName = 'swapETHForExactTokens'
           // (uint amountOut, address[] calldata path, address to, uint deadline)
-          args = [amountOut, path, to, deadline]
+          args = [amountOut, path, to, deadline, challengeKey.toHexString()]
           value = amountIn
         } else if (etherOut) {
           methodName = 'swapTokensForExactETH'
           // (uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
-          args = [amountOut, amountIn, path, to, deadline]
+          args = [amountOut, amountIn, path, to, deadline, challengeKey.toHexString()]
           value = ZERO_HEX
         } else {
           methodName = 'swapTokensForExactTokens'
           // (uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
-          args = [amountOut, amountIn, path, to, deadline]
+          args = [amountOut, amountIn, path, to, deadline, challengeKey.toHexString()]
           value = ZERO_HEX
         }
         break
